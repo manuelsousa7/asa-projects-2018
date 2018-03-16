6622e744b1ff2710cc75f705c55625fbdd57cb43
Manuel Sousa <manuelvsousa@tecnico.ulisboa.pt>
2018-03-16 22:15:45 +0000

    formated

--- project1/P1.cpp
+++ project1/P1.cpp
@@ -2,15 +2,14 @@
 #include <algorithm>
 using namespace std;
 
-
 #define UNVISITED -1
 #define INF numeric_limits<int>::max()
 
 vector<vector<int> > G;
 vector<int> num, low, S, visited, sccG;
-int SCC, N, M, dfsNC,f = 1,connected;
-typedef vector <int>::iterator vi;
-unordered_map<int,int> small;
+int SCC, N, M, dfsNC, f = 1, connected;
+typedef vector<int>::iterator vi;
+unordered_map<int, int> small;
 
 void tarjan(int u) {
   low[u] = num[u] = dfsNC++;
@@ -19,28 +18,27 @@ void tarjan(int u) {
   for (int j = 0; j < (int)G[u].size(); j++) {
     int v = G[u][j];
     if (num[v] == UNVISITED) {
-			tarjan(v);
-		}
+      tarjan(v);
+    }
 
-    if (visited[v])
-			low[u] = min(low[u], low[v]);
+    if (visited[v]) low[u] = min(low[u], low[v]);
   }
 
   if (low[u] == num[u]) {
     SCC++;
-		int min = INF;
+    int min = INF;
     while (1) {
       int v = S.back();
       S.pop_back();
       visited[v] = 0;
-			sccG[v] = SCC;
-			min = v < min ? v : min;
+      sccG[v] = SCC;
+      min = v < min ? v : min;
       if (u == v) {
-				small.insert(make_pair(SCC,min));
-				break;
-			}
+        small.insert(make_pair(SCC, min));
+        break;
+      }
     }
-  } 
+  }
 }
 
 int main() {
@@ -51,7 +49,7 @@ int main() {
   visited = vector<int>(N + 1, 0);
   num = vector<int>(N + 1, UNVISITED);
   low = vector<int>(N + 1, 0);
-	sccG = vector<int>(N + 1, -1);
+  sccG = vector<int>(N + 1, -1);
 
   for (int i = 0; i < M; i++) {
     int x, y;
@@ -59,21 +57,21 @@ int main() {
     G[x - 1].push_back(y - 1);
   }
   for (int i = 0; i < N; i++)
-    if (num[i] == UNVISITED) 
-			tarjan(i);
+    if (num[i] == UNVISITED) tarjan(i);
 
-	cout << SCC << endl;
-	set<pair<int,int> > connections;
-	for (int u = 0; u < N; u++)
-    for (vi v = G[u].begin(); v != G[u].end(); v++) 
-			if(sccG[*v] != sccG[u]){
-				connections.insert(make_pair(small.find(sccG[u])->second + 1,small.find(sccG[*v])->second + 1));
-			}
+  cout << SCC << endl;
+  set<pair<int, int> > connections;
+  for (int u = 0; u < N; u++)
+    for (vi v = G[u].begin(); v != G[u].end(); v++)
+      if (sccG[*v] != sccG[u]) {
+        connections.insert(make_pair(small.find(sccG[u])->second + 1,
+                                     small.find(sccG[*v])->second + 1));
+      }
 
-	cout << connections.size() << endl;
-	for (auto elem : connections) {
-		cout << elem.first << " " << elem.second << endl;
-	}
-	
+  cout << connections.size() << endl;
+  for (auto elem : connections) {
+    cout << elem.first << " " << elem.second << endl;
+  }
+
   return 0;
 }
